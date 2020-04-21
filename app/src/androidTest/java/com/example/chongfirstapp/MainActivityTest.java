5488df5f8b4eceaebcf18c136b1c97394094ae0d
EricaC <Char-wae.Chong@seatlecolleges.edu>
2020-04-20 23:00:26 -0700

    first app

---
+++ app/src/androidTest/java/com/example/chongfirstapp/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.chongfirstapp;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+        @Rule
+        public ActivityScenarioRule<MainActivity>activityScenarioRule
+                = new ActivityScenarioRule<>(MainActivity.class);
+}
