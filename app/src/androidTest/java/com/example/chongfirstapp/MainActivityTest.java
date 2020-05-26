7a8aeb5baee5d92320120cc4a39f389c1e118730
EricaC <Char-wae.Chong@seatlecolleges.edu>
2020-05-26 14:27:11 -0700

    updated Week 3 assignment code

--- app/src/androidTest/java/com/example/chongfirstapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/chongfirstapp/MainActivityTest.java
@@ -4,14 +4,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -19,22 +12,6 @@ public class MainActivityTest {
         @Rule
         public ActivityScenarioRule<MainActivity>activityScenarioRule
                 = new ActivityScenarioRule<>(MainActivity.class);
-
-        @Test
-        public void canMoveToSecondActivity(){
-            onView(withId(R.id.nameEditText))
-                    .perform(typeText(Constants.KEY_TEST_NAME),closeSoftKeyboard());
-            onView(withId(R.id.ageInput))
-                    .perform(typeText(Constants.KEY_TEST_AGE), closeSoftKeyboard());
-            onView(withId(R.id.occupationInput))
-                    .perform(typeText(Constants.KEY_TEST_OCCUPATION),closeSoftKeyboard());
-            onView(withId(R.id.descriptionInput))
-                    .perform(typeText(Constants.KEY_TEST_DESCRIPTION), closeSoftKeyboard());
-            onView(withId(R.id.submitButton))
-                    .perform(click());
-        }
-
-
 
 
 
--- app/src/androidTest/java/com/example/chongfirstapp/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/chongfirstapp/SecondActivityTest.java
@@ -4,13 +4,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 
@@ -19,13 +13,4 @@ public class SecondActivityTest {
     @Rule
     public ActivityScenarioRule<SecondActivity>activityScenarioRule
             = new ActivityScenarioRule<>(SecondActivity.class);
-
-    @Test
-    public void infoMatchesWithMainActivityInput(){
-        onView(withId(R.id.name)).check(matches(withText(Constants.KEY_TEST_NAME)));
-        onView(withId(R.id.age)).check(matches(withText(Constants.KEY_TEST_AGE)));
-        onView(withId(R.id.occupation)).check(matches(withText(Constants.KEY_TEST_OCCUPATION)));
-        onView(withId(R.id.description)).check(matches(withText(Constants.KEY_TEST_DESCRIPTION)));
-    }
-
 }
