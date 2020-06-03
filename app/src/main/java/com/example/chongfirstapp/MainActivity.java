117cb2cbac271de7e24ac655629bd4d66bd59388
EricaC <Char-wae.Chong@seatlecolleges.edu>
2020-06-02 23:33:25 -0700

    updated code for Assignment 7

--- app/src/main/java/com/example/chongfirstapp/MainActivity.java
+++ app/src/main/java/com/example/chongfirstapp/MainActivity.java
@@ -3,56 +3,132 @@ package com.example.chongfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG= null;
     private EditText Name;
     private EditText UserName;
     private EditText Email;
-    private EditText birthDate;
+    private TextView birthDate;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        final SettingListAdapter adapter = new SettingListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        TabLayout tabs =  findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Profile"));
+        tabs.addTab(tabs.newTab().setText("Matches"));
+        tabs.addTab(tabs.newTab().setText("Settings"));
+        tabs.setupWithViewPager(viewPager);
 
         Name = findViewById(R.id.nameEditText);
         UserName = findViewById(R.id.usernameEditText);
-        Email = findViewById(R.id.emailEditText);
+        Email= findViewById(R.id.emailEditText);
         birthDate = findViewById(R.id.getDate);
 
-    }
+        FirebaseFirestore getMatchesDB = FirebaseFirestore.getInstance();
 
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if(savedInstanceState.containsKey(Constants.KEY_BIRTH_DATE)){
-            birthDate.setText((String)savedInstanceState.get(Constants.KEY_BIRTH_DATE));
-        }
-    }
+        getMatchesDB.collection("matches").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        Log.d(TAG, document.getId()+ " => " + document.getData());
+                    }
+                }else{
+                    Log.d(TAG, "Error: ", task.getException());
+                }
+            }
+        });
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(Constants.KEY_BIRTH_DATE, birthDate.getText().toString());
     }
 
 
     public void showBirthDateDialog(View v){
         DialogFragment newFragment = new DateDialog(birthDate);
         newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+    private void setupViewPager(ViewPager viewPager)
+    {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ProfileFragment(),"Profile");
+        adapter.addFragment(new MatchesFragment(),"Matches");
+        adapter.addFragment(new SettingsFragment(),"Settings");
+        viewPager.setAdapter(adapter);
+    }
+
+
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+
+        public Adapter(FragmentManager manager)
+        {
+            super(manager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        }
+        @Override
+        public Fragment getItem(int position){
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount(){
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String tabName){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(tabName);
+        }
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
     }
 
 
@@ -78,6 +154,8 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+
 }
 
 
--- app/src/main/java/com/example/chongfirstapp/SettingsFragment.java
+++ app/src/main/java/com/example/chongfirstapp/SettingsFragment.java
@@ -1,27 +1,68 @@
 package com.example.chongfirstapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SettingsFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.Nullable;
+
+public class SettingsFragment extends Fragment {
+    private List<Settings> sItemList;
     private SettingListAdapter sDB;
     private SettingsViewModel sViewModel;
 
+    public void setSettingsList(List<Settings> getSettingsList){
+        if(sItemList == null){
+            sItemList = new ArrayList<>();
+        }
+        sItemList.clear();
+        sItemList.addAll(getSettingsList);
+
+        if(sDB != null){
+            sDB.setSettingItem(getSettingsList);
+        }
+
+    }
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view,
+
+
+        View view=  inflater.inflate(R.layout.settings_view,
                 container, false);
 
-        return recyclerView;
+        Context context = view.getContext();
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        if(sItemList != null){
+            sDB.setSettingItem(sItemList);
+        }
+        recyclerView.setAdapter(sDB);
+        return view;
     }
-    public void onViewCreated(View view, Bundle savedInstanceState){
-
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        sViewModel= new ViewModelProvider(this).get(SettingsViewModel.class);
+        sViewModel.getAllSettings().observe(getViewLifecycleOwner(), new Observer<List<Settings>>() {
+            @Override
+            public void onChanged(List<Settings> settings) {
+                if(settings != null){
+                    setSettingsList(settings);
+                }
+            }
+        });
     }
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public ViewHolder(LayoutInflater inflater, ViewGroup parent){
